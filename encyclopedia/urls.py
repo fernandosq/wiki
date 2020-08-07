0bfd01aab3fd446a4d1acab448bd8a1cd705ac50
fernando.sq <mastmaster@hotmail.com>
2020-08-07 11:40:37 +0200

    Create the views create,edit,search, withe the html incluide

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("wiki/<str:entry_name>", views.entry, name="entry"),
     path("create", views.create, name="create"),
     path("edit/<str:title>", views.edit, name="edit"),
-    path("wiki/", views.randomPage, name="random")
+    path("random", views.randomPage, name="random")
 ]
